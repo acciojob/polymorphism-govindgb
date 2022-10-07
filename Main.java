6d6331a95ece160ad827532d195eaada594ae250
govindgb <90210096+govindgb@users.noreply.github.com>
2022-10-07 20:26:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+class product
+{
+    public int product(int x, int y) {
+      return 0;
+    } 
+    public int product(int x, int y,int z) {
+        return 0;
+    } 
+    public double product(double x, double y) {
+        return 0.0;
+    } 
+    
+}
+
+class Main{
+    public static void main(String[] args) {
+        product p = new product();
+
+        p.product(0, 0);
+        p.product(0, 0,0);
+        p.product(0.0, 0.0);
+    }
+}

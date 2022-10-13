502d784e9863609cef9d5bbabdbdac99b85baa59
govindgb <govindbhattpnd@gmail.com>
2022-10-13 14:35:03 +0530

    polymorphism

--- Main.java
+++ Main.java
@@ -1,25 +1,24 @@
-class product
-{
-    public int product(int x, int y) {
-      return 0;
-    } 
-    public int product(int x, int y,int z) {
-        return 0;
-    } 
-    public double product(double x, double y) {
-        return 0.0;
-    } 
-    
+package com.driver;
+
+public class Main {
+public static void main(String[] args){
+    Product p=new Product();
+    int ans=p.product(1,2);
+
+            int ans1=p.product(1,2,3);
+                    double ans2=p.product(1.2,1.4);
+    System.out.println(ans+ " "+ ans1+" "+ans2);
+    //System.out.println();
+   // System.out.println();
 }
-
-class Main{
-    public static void main(String[] args) {
-        product p = new product();
-
-        p.product(0, 0);
-
-        p.product(0, 0,0);
-        
-        p.product(0.0, 0.0);
+public static class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x,double y){
+        return x*y;
     }
 }
